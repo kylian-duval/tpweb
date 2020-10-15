16520cca51708617fd992f561034b05350b3e649
kylian-duval <kduval@la.providence.net>
2020-10-15 11:56:39 +0200

    exercice 7 et 8 php fini

--- exercicephp/exercice7.php
+++ exercicephp/exercice7.php
@@ -6,32 +6,29 @@
     <title>Document</title>
 </head>
 <body>
-    <?php session_start();
-    if(isset($_POST['Deconnexion'])){
-        if(isset($_SESSION["test"])){
-            session_destroy();
-          echo '<script> window.location.reload() </script>';
-        }
-        }
-    
-    ?>
-
     <form action="" method="post">
         <input type="text" name="text" id="">
         <input type="submit" value="Valide" name="valide">
-        <input type="submit" value="Deconnexion" name="deconnexion " href="javascript:window.location.reload()">
+        <form action="" method="post"> <input type="submit" value="Deconnexion" name="deco" ></form>
     </form>
-    <?php
+    <?php session_start(); 
         if(isset($_POST['valide'])){
             $_SESSION["test"]=$_POST["text"];
             echo '<p style="color: red;">'.$_SESSION["test"].'</p>';
         }elseif(isset($_SESSION["test"])){
             echo '<p style="color: red;">'.$_SESSION["test"].'</p>';
         }
-
-
+        
+            if(isset($_POST['deco'])){   
+                session_destroy();
+                echo '<p><span> je suis déco </span></p>';
+            }
+        
+        
+        
         highlight_file((__FILE__))
-    ?>
+        ?>
+        
     <p style="color: red;"></p>
 </body>
 </html>
